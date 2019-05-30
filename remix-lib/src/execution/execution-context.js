2620ca9b1eeaaea34d8d5e2b361a245b675652ed
BillyQin <965824981@qq.com>
2019-05-30 19:47:20 +0800

    change simplechainjs-util and simplechainjs-vm version

--- remix-lib/src/execution/execution-context.js
+++ remix-lib/src/execution/execution-context.js
@@ -2,9 +2,9 @@
 'use strict'
 var Web3 = require('web3')
 var EventManager = require('../eventManager')
-var EthJSVM = require('ethereumjs-vm')
+var EthJSVM = require('simplechainjs-vm')
 var ethUtil = require('ethereumjs-util')
-var StateManager = require('ethereumjs-vm/dist/stateManager')
+var StateManager = require('simplechainjs-vm/dist/stateManager')
 var Web3VMProvider = require('../web3Provider/web3VmProvider')
 
 var rlp = ethUtil.rlp
